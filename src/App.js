0942c95320f679c890e8c090bd67abd3a100ab37
omare98 <36833774+omare98@users.noreply.github.com>
2021-05-12 16:30:00 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Hello from "./pages/components/Hello";
 import MyComponent from "./pages/components/MyComponent";
 import Counter from "./pages/components/Counter";
@@ -9,15 +11,56 @@ import User from "./pages/components/User";
 
 function App() {
   return (
-    <div className="App">
-      {/* <MyComponent name="리액트"></MyComponent>
-      <Hello></Hello>
-      <Counter></Counter>
-      <Animals></Animals>
-      <ExpensiveComponent></ExpensiveComponent> */}
-      <AsyncCounter></AsyncCounter>
-      <User></User>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/hello">Hello</Link>
+          </li>
+          <li>
+            <Link to="/mycomponent">MyComponent</Link>
+          </li>
+          <li>
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link to="/animals">Animals</Link>
+          </li>
+          <li>
+            <Link to="/expensivecomponent">ExpensiveComponent</Link>
+          </li>
+          <li>
+            <Link to="/asynccounter">AsyncCounter</Link>
+          </li>
+          <li>
+            <Link to="/user">User</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/hello">
+            <Hello />
+          </Route>
+          <Route exact path="/mycomponent">
+            <MyComponent name="리액트"></MyComponent>
+          </Route>
+          <Route exact path="/counter">
+            <Counter />
+          </Route>
+          <Route exact path="/animals">
+            <Animals />
+          </Route>
+          <Route exact path="/expensivecomponent">
+            <ExpensiveComponent />
+          </Route>
+          <Route exact path="/asynccounter">
+            <AsyncCounter />
+          </Route>
+          <Route exact path="/user">
+            <User />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
